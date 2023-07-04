1da92b2014020b4c1273fcc14ad47fce3371938a
Elisafa <elisafajunior@hotmail.com>
2023-07-03 21:34:11 -0300

    first commit

---
+++ exercicio.js
@@ -0,0 +1,7 @@
+"use strict";
+const multiplicar = (valor1, valor2) => {
+    return valor1 * valor2;
+};
+const saudacao = (nome) => {
+    return `Olá ${nome}`;
+};
---
+++ exercicio.ts
@@ -0,0 +1,7 @@
+const multiplicar = (valor1: number, valor2: number) => {
+  return valor1 * valor2;
+};
+
+const saudacao = (nome: string) => {
+  return `Olá ${nome}`;
+};
